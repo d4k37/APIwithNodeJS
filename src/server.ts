ffad7ea3308933e59dece5b0d136cd8db6aecdfc
Luís <59730229+d4k37@users.noreply.github.com>
2021-05-01 23:52:19 -0300

    up

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
+/* eslint-disable linebreak-style */
 import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => response.json({ message: 'ola' }));
+
+app.listen(3333, () => console.log('rodando'));
