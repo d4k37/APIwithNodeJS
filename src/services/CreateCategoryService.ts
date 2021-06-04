93c734ad38d0a7cd4a01198cc9e5f3aeb79e6c42
Luís <59730229+d4k37@users.noreply.github.com>
2021-06-03 23:28:49 -0300

    up

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,35 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+
+
+interface IRequest{
+    name: string;
+    description: string;
+}
+
+class CreateCategoryService{
+
+constructor(private categoriesRepository: ICategoriesRepository){
+    
+}
+
+    execute({name, description}: IRequest): void{
+
+
+        const categoriesRepository = new CategoriesRepository();
+        const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+
+ if (categoryAlreadyExists) {
+     throw new Error("CATEGORIA JÁ CRIADA")
+  
+ }
+   this.categoriesRepository.create({ name, description });
+    }
+
+
+}
+
+
+export {CreateCategoryService}
